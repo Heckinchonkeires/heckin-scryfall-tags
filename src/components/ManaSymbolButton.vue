9d3511487ab41d6479d046e237ab454dfd437f46
Heckinchonkeires <heckinchonkeires@gmail.com>
2023-08-23 18:37:57 -0500

    Started on some mana symbol buttons. Tickled the Scryfall API.

---
+++ src/components/ManaSymbolButton.vue
@@ -0,0 +1,40 @@
+<template>
+  <img
+    @click="onClick()"
+    :src="require(`../assets/${symbolName}`)"
+    alt="A mana symbol"
+    :class="btnActive ? '' : 'inactive'"
+  />
+</template>
+
+<script>
+export default {
+  name: "ManaSymbolButton",
+  props: {
+    symbolName: String,
+    active: Boolean,
+  },
+  data() {
+    return {
+      btnActive: false,
+    };
+  },
+  methods: {
+    onClick() {
+      this.$emit("mana-symbol-click");
+      this.btnActive = !this.btnActive;
+    },
+  },
+};
+</script>
+
+<style scoped>
+img {
+  width: 100px;
+  height: 100px;
+}
+
+.inactive {
+  opacity: 50%;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,75 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <ManaSymbolButton
+      v-for="symbol in manaSymbols"
+      :key="symbol.id"
+      :symbolName="symbol.src"
+    />
   </div>
+  <SubmitButton
+    @submit-btn-click="submitBtnClick"
+    :text="'submit'"
+    :color="'lightblue'"
+  />
+  <img :src="randomCard" alt="" />
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import ManaSymbolButton from "@/components/ManaSymbolButton.vue";
+import SubmitButton from "@/components/SubmitButton.vue";
 
 export default {
   name: "HomeView",
   components: {
-    HelloWorld,
+    ManaSymbolButton,
+    SubmitButton,
+  },
+  data() {
+    return {
+      manaSymbols: [
+        {
+          id: 1,
+          src: "W.png",
+          name: "white",
+        },
+        {
+          id: 2,
+          src: "U.png",
+          name: "white",
+        },
+        {
+          id: 3,
+          src: "B.png",
+          name: "white",
+        },
+        {
+          id: 4,
+          src: "R.png",
+          name: "white",
+        },
+        {
+          id: 5,
+          src: "G.png",
+          name: "white",
+        },
+        {
+          id: 6,
+          src: "C.png",
+          name: "white",
+        },
+      ],
+      randomCard: null,
+    };
+  },
+  methods: {
+    async submitBtnClick() {
+      const res = await fetch(
+        "https://api.scryfall.com/cards/random?pretty=true"
+      );
+      const data = await res.json();
+      this.randomCard = data.image_uris.small;
+    },
   },
 };
 </script>
